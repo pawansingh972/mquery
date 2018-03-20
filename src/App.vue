dd086afbd567c855fd0ec2c8ffd7bf03bf3ea719
Chetan <chetan.narian@gmail.com>
2018-03-20 21:03:57 +0530

    Initial commit
    1. Landing Page

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  html, body {
+    font-family: 'Roboto', sans-serif;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    color: #263646;
+    background-color: #fdfdfd;
+  }
+
+  #app {
+    height: 100%;
+  }
+</style>
---
+++ src/components/Query.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="query">
+    <h1>m{query} - Your mongoDB query generator</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'query',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .query {
+    display: table;
+    height: 100%;
+    width: 100%;
+    text-align: center;
+  }
+  h1 {
+    display: table-cell;
+    vertical-align: middle;
+  }
+</style>
