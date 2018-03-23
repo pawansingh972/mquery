8634a1b868fd8f8329b12b7c685ca990c9179b40
Chetan <chetan.narian@gmail.com>
2018-03-23 23:28:47 +0530

    Version 1 -UI Enhancements

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,23 @@ export default {
   html, body {
     font-family: 'Roboto', sans-serif;
     height: 100%;
-    margin: 0;
+    margin: 0 auto;
     padding: 0;
-    color: #263646;
+    color: #343a40;
     background-color: #fdfdfd;
   }
 
   #app {
     height: 100%;
   }
+
+  #header {
+    box-shadow: 0px 5px 10px 0px rgba(0,64,128,0.05);
+    height: 65px;
+  }
+
+  #header h4 {
+    line-height: 65px;
+  }
+
 </style>
